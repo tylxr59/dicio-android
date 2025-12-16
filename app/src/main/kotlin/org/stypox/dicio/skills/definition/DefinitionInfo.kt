2c271a323e805a6131b712b858eb78e5d71c59f5
tylxr <102394635+tylxr59@users.noreply.github.com>
2025-12-15 21:19:10 -0500

    Ope... forgot the category for it

--- app/src/main/kotlin/org/stypox/dicio/skills/definition/DefinitionInfo.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/definition/DefinitionInfo.kt
@@ -12,6 +12,8 @@ import org.stypox.dicio.R
 import org.stypox.dicio.sentences.Sentences
 
 object DefinitionInfo : SkillInfo("definition") {
+    override val categoryNameRes = R.string.category_information
+
     override fun name(context: Context) =
         context.getString(R.string.skill_name_definition)
 
